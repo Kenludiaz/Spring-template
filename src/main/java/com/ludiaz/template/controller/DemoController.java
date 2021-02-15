74996bed8db3a2c0cb5e6af7b22c473b5e887a1e
Kenneth Lu Diaz <ludiazken@gmail.com>
2021-02-15 09:07:32 -0500

    Creates template

---
+++ src/main/java/com/ludiaz/template/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.ludiaz.template.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+    @ResponseBody
+    @GetMapping("/demo")
+    public String demo() {
+        return "Hello Demo";
+    }
+}
